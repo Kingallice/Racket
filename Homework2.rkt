34b747043d71e17f8a5dbef0c2f371c809b92e1e
Noah <noahmosel73@gmail.com>
2023-02-07 13:17:23 -0600

    Finished HW2

--- Homework2.rkt
+++ Homework2.rkt
@@ -23,6 +23,10 @@
 (printf "(same-digits 7361 7136) = ~a\n" (same-digits 7361 7136))
 
 ;Problem 2
+;Problems -> The algorithm doesnt allow for odd lengths
+;  So, if you have a number that is 5 digits long and
+;  square it you will get a number with 10 digits, however,
+;  there is no middle of the number. 
 (define (middle-square prev len)
   (define (get-list number)
     (string->list (number->string (expt number 2))))
@@ -40,4 +44,22 @@
 
 (printf "(middle-square 5772156649 10) = ~a\n" (middle-square 5772156649 10))
 (printf "(middle-square 46283 6) = ~a\n" (middle-square 46283 6))
+
+; Problem 3
+(define (next-mersenne n)
+  (define (is-prime num test)
+    (if (> (sqrt num) test)
+        (cond
+          [(zero? (modulo num test)) #f]
+          [(positive? (modulo num test)) (is-prime num (+ test 1))])
+        #t))
+
+  (cond
+    [(boolean=? #t (is-prime n 2))
+     (cond
+       [(boolean=? #t (is-prime (sub1 (expt 2 n)) 2)) n]
+       [(boolean=? #f (is-prime (sub1 (expt 2 n)) 2)) (next-mersenne (add1 n))])]
+    [(boolean=? #f (is-prime n 2)) (next-mersenne (add1 n))]))
+
+(printf "(next-mersenne 20) = ~a\n" (next-mersenne 20))
   
