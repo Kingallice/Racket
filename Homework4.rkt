00701044fe7b50b4d661b337ca4944a535136c53
Noah <noahmosel73@gmail.com>
2023-02-15 16:39:02 -0600

    Started problem 1

--- Homework4.rkt
+++ Homework4.rkt
@@ -1,2 +1,12 @@
 #lang racket
+(define (accumulate operator null list)
+  (if (null? list)
+      null
+      (operator (car list)
+                (accumulate operator null (cdr list)))))
+
 ; Problem 1
+(define (contains? list object)
+  (accumulate (lambda (first last) (+ first last) 10 10) 0 (list)))
+
+(contains? (list 5 2 4 1) 5)
