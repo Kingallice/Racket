2cdd5b7a786daeed8b6553786bdcf30345676114
Noah <noahmosel73@gmail.com>
2023-04-23 17:28:57 -0500

    Update Homework9.rkt

--- Homework9.rkt
+++ Homework9.rkt
@@ -5,37 +5,62 @@
   [ add (lhs FAE?) (rhs FAE?)]
   [ id (name symbol?)]
   [ fun (param symbol?) (body FAE?)]
-  [ app (fun-expr FAE?) (arg-expr FAE?)])
+  [ app (fun-expr FAE?) (arg-expr FAE?)]
+  [ newbox (expr FAE?)]
+  [ setbox (expr1 FAE?) (expr2 FAE?)]
+  [ openbox (expr FAE?)]
+  [ seqn (expr1 FAE?) (expr2 FAE?)])
 
 (define-type FAE-Value
   [ numV (n number?)]
   [ closureV (param symbol?)
              (body FAE?)
-             (ds DefrdSub?)])
+             (ds DefrdSub?)]
+  [ boxV (location number?)])
+
+(define-type ValueXStore
+  [ vxs (value FAE-Value?) (store Store?)])
 
 (define-type DefrdSub
   [ mtSub ]
   [ aSub (name symbol?) (value FAE-Value?) (ds DefrdSub?)])
 
+(define-type Store
+  [ mtSto ]
+  [ aSto (location number?) (value FAE-Value?) (store Store?)])
+
 ;; lookup : symbol DefrdSub â†’ FAE-Value
-(define (lookup name ds)
+(define (ds-lookup name ds)
   (type-case DefrdSub ds
-    [ mtSub () (error 'lookup "no binding for identifier" )]
+    [ mtSub () (error 'ds-lookup "no binding for identifier" )]
     [ aSub (bound-name bound-value rest-ds)
            (if (symbol=? bound-name name)
                bound-value
-               (lookup name rest-ds))]))
+               (ds-lookup name rest-ds))]))
+
+(define (store-lookup loc-index sto)
+  (type-case Store sto
+    [ mtSto () (error 'store-lookup "no value at location" )]
+    [ aSto (location value rest-store)
+           (if (= location loc-index)
+               value
+               (store-lookup loc-index rest-store))]))
 
 ;; num+ : numV numV âˆ’â†’ numV
 (define (num+ n1 n2)
   ( numV (+ (numV-n n1) (numV-n n2))))
 
 ;; interp : FAE DefrdSub â†’ FAE-Value
-(define (interp expr ds)
+(define (interp expr ds store)
   (type-case FAE expr
-    [ num (n) ( numV n)]
-    [ add (l r) (num+ (interp l ds) (interp r ds))]
-    [ id (v) (lookup v ds)]
+    [ num (n) (vxs ( numV n) store)]
+    [ id (v) (vxs (store-lookup (ds-lookup v ds) store) store)]
+    [ add (l r) 
+          (type-case ValueXStore (interp l ds store)
+            [ vxs (l-value l-store)
+                (type-case ValueXStore (interp r ds l-store)
+                  [ vxs (r-value r-store)
+                    (vxs (num+ l-value r-value) r-store)])])]
     [ fun (bound-id bound-body)
           ( closureV bound-id bound-body ds)]
     [ app (fun-expr arg-expr)
@@ -43,7 +68,13 @@
             (interp (closureV-body fun-val)
                     ( aSub (closureV-param fun-val)
                            (interp arg-expr ds)
-                           (closureV-ds fun-val))))]))
+                           (closureV-ds fun-val))))]
+    [ newbox (value-expr)
+          (type-case ValueXStore (interp value-expr ds store)
+            [vxs (expr-value expr-store)
+                (local ([define new-loc (next-location store)])
+                    (vxs (boxV new-loc)
+                          (aSto new-loc expr-value expr-store)))])]))
 
 (define (parse sexp)
   (cond
