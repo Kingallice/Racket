29d7aeeef6bc9fcd0ed0501cca9dc77b092de385
Noah <noahmosel73@gmail.com>
2023-04-23 22:43:58 -0500

    finished HW9

--- Homework9.rkt
+++ Homework9.rkt
@@ -1,15 +1,27 @@
 #lang plai
 
+(define (num-zero? n)
+  (zero? (numV-n n)))
+
+(define next-location
+  (local ([define last-loc (box -1)])
+    (lambda (store)
+      (begin
+        (set-box! last-loc (+ 1 (unbox last-loc)))
+        (unbox last-loc)))))
+
 (define-type FAE
   [ num (n number?)]
   [ add (lhs FAE?) (rhs FAE?)]
   [ id (name symbol?)]
+  [ if0 (c FAE?) (t FAE?) (e FAE?) ]
   [ fun (param symbol?) (body FAE?)]
   [ app (fun-expr FAE?) (arg-expr FAE?)]
   [ newbox (expr FAE?)]
   [ setbox (expr1 FAE?) (expr2 FAE?)]
   [ openbox (expr FAE?)]
-  [ seqn (expr1 FAE?) (expr2 FAE?)])
+  [ seqn (expr1 FAE?) (expr2 FAE?)]
+  [ setvar (var symbol?) (val FAE?)])
 
 (define-type FAE-Value
   [ numV (n number?)]
@@ -23,7 +35,7 @@
 
 (define-type DefrdSub
   [ mtSub ]
-  [ aSub (name symbol?) (value FAE-Value?) (ds DefrdSub?)])
+  [ aSub (name symbol?) (value number?) (ds DefrdSub?)])
 
 (define-type Store
   [ mtSto ]
@@ -52,9 +64,16 @@
 
 ;; interp : FAE DefrdSub â†’ FAE-Value
 (define (interp expr ds store)
+  ;(printf "EXPR: ~a\nDS: ~a\nSTORE: ~a\n\n" expr ds store)
   (type-case FAE expr
-    [ num (n) (vxs ( numV n) store)]
+    [ num (n) (vxs (numV n) store)]
     [ id (v) (vxs (store-lookup (ds-lookup v ds) store) store)]
+    [ if0 (c t e) 
+          (type-case ValueXStore (interp c ds store)
+            [vxs (test-value test-store)
+                (if (num-zero? test-value) 
+                  (interp t ds test-store) 
+                  (interp e ds test-store))])]
     [ add (l r) 
           (type-case ValueXStore (interp l ds store)
             [ vxs (l-value l-store)
@@ -62,19 +81,48 @@
                   [ vxs (r-value r-store)
                     (vxs (num+ l-value r-value) r-store)])])]
     [ fun (bound-id bound-body)
-          ( closureV bound-id bound-body ds)]
+          (vxs (closureV bound-id bound-body ds) store)]
     [ app (fun-expr arg-expr)
-          (local ([define fun-val (interp fun-expr ds)])
-            (interp (closureV-body fun-val)
-                    ( aSub (closureV-param fun-val)
-                           (interp arg-expr ds)
-                           (closureV-ds fun-val))))]
+          (type-case ValueXStore (interp fun-expr ds store)
+            [vxs (fun-val fun-store)
+                (type-case ValueXStore (interp arg-expr ds fun-store)
+                  [vxs (arg-val arg-store)
+                      (local ([define new-loc (next-location arg-store)])
+                        (interp (closureV-body fun-val)
+                                (aSub (closureV-param fun-val)
+                                      new-loc (closureV-ds fun-val))
+                                (aSto new-loc arg-val arg-store)))])])]
+    [ seqn (e1 e2)
+          (type-case ValueXStore (interp e1 ds store)
+            [vxs (e1-value e1-store)
+                (interp e2 ds e1-store)])]
     [ newbox (value-expr)
           (type-case ValueXStore (interp value-expr ds store)
             [vxs (expr-value expr-store)
-                (local ([define new-loc (next-location store)])
+                (local ([define new-loc (next-location expr-store)])
                     (vxs (boxV new-loc)
-                          (aSto new-loc expr-value expr-store)))])]))
+                          (aSto new-loc expr-value expr-store)))])]
+    [ setbox (box-expr value-expr)
+          (type-case ValueXStore (interp box-expr ds store)
+            [vxs (box-value box-store)
+                (type-case ValueXStore (interp value-expr ds box-store)
+                  [vxs (value-value value-store)
+                      (vxs value-value 
+                          (aSto (boxV-location box-value)
+                            value-value
+                            value-store))])])]
+    [ openbox (box-expr)
+          (type-case ValueXStore (interp box-expr ds store)
+            [vxs (box-value box-store)
+                (vxs (store-lookup (boxV-location box-value)
+                          box-store)
+                    box-store)])]
+    [setvar (var value)
+          (type-case ValueXStore (interp value ds store)
+            [vxs (value-value value-store)
+              (local ([define the-loc (ds-lookup var ds)])
+                (vxs value-value
+                  (aSto the-loc value-value value-store)))])]))
 
 (define (parse sexp)
   (cond
@@ -86,27 +134,33 @@
        [(+) (add (parse (second sexp))
                  (parse (third sexp)))]
 ; modify by parsing the following into an app with multiple params
-;       [(with) (with (first (second sexp))
-;                     (parse (second (second sexp)))
-;                     (parse (third sexp)))
-;               ]
+       [(with) (app 
+                     (fun (first (second sexp)) (parse (third sexp)))
+                     (parse (second (second sexp))))
+               ]
+       [(if0) (if0 (parse (second sexp)) (parse (third sexp)) (parse (fourth sexp)))]
 ; modify by parsing the following to handle multiple params
        [(fun) (fun 
                (first (second sexp))
                (parse (third sexp))
                )
               ]
-       [else
-        (cond ((list? (first sexp))  ;; this cond may not be needed
+       [(seqn) (seqn (parse (second sexp)) (parse (third sexp)))]
+       [(newbox) (newbox (parse (second sexp)))]
+       [(setbox) (setbox (parse (second sexp)) (parse (third sexp)))]
+       [(openbox) (openbox (parse (second sexp)))]
+       [(setvar) (setvar (second sexp) (parse (third sexp)))]
+       [ else
+;; this cond may not be needed
 ; modify by parsing the following to handle multiple params
-               (app (parse (first sexp)) (parse (second sexp)))))
-        ]
-       )]))
+               (app (parse (first sexp)) (parse (second sexp)))
+        ])]))
 
 ;; testing the interpreter on a function application (single param)
 ;(interp (parse '{{fun {x} {+ x x}} 5}) (mtSub) )
 
 ;; Part 1 Test Code
+  (printf "\nPart 1 Test\n")
   (test (parse '{with {b {newbox 0}}
                     {seqn {setbox b {+ 1 {openbox b}}}
                           {openbox b}}})
@@ -211,6 +265,7 @@
             (aSto 5 (boxV 4) (aSto 4 (numV 0) (mtSto))))))))))
 
 ;;Part 2 Test Code
+  (printf "\nPart 2 Test\n")
   (test (parse '{with {y 0}
                     {seqn
                      {seqn
